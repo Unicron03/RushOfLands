f5c50cabab9ec7c2759845d46a197558c73a195c
Unicron03 <110382392+Unicron03@users.noreply.github.com>
2024-01-20 11:04:03 +0100

    For update language

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.title = "Rush Of Lands"
+    t.vesrion = "11.3"
+    t.console = false
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 0
+    t.window.resizable = false
+    t.window.borderless = true
+end
